fbc6679bf4c5233e662ae485c8c36afee9d441e3
Sir-Muguna <mwendasir@gmail.com>
2025-10-03 13:11:52 +0300

    correct stg_medicare model

--- dbt_project/models/staging/stg_medicare_data.sql
+++ dbt_project/models/staging/stg_medicare_data.sql
@@ -16,22 +16,63 @@ with raw_data as (
         trim(brnd_name) as brnd_name,
         trim(gnrc_name) as gnrc_name,
         
-        -- Prescription volume metrics (converted from string to proper types)
-        nullif(try_cast(tot_clms as integer), '') as tot_clms,
-        nullif(try_cast(tot_benes as integer), '') as tot_benes,
-        nullif(try_cast(tot_prscrbrs as integer), '') as tot_prscrbrs,
-        nullif(try_cast(tot_30day_fills as float), '') as tot_30day_fills,
+        -- Prescription volume metrics (with better empty string handling)
+        case when nullif(trim(tot_clms), '') is not null 
+             then try_cast(tot_clms as integer) 
+             else null 
+        end as tot_clms,
         
-        -- Cost metrics (converted from string to float)
-        nullif(try_cast(tot_drug_cst as float), '') as tot_drug_cst,
-        nullif(try_cast(lis_bene_cst_shr as float), '') as lis_bene_cst_shr,
-        nullif(try_cast(nonlis_bene_cst_shr as float), '') as nonlis_bene_cst_shr,
+        case when nullif(trim(tot_benes), '') is not null 
+             then try_cast(tot_benes as integer) 
+             else null 
+        end as tot_benes,
+        
+        case when nullif(trim(tot_prscrbrs), '') is not null 
+             then try_cast(tot_prscrbrs as integer) 
+             else null 
+        end as tot_prscrbrs,
+        
+        case when nullif(trim(tot_30day_fills), '') is not null 
+             then try_cast(tot_30day_fills as float) 
+             else null 
+        end as tot_30day_fills,
+        
+        -- Cost metrics
+        case when nullif(trim(tot_drug_cst), '') is not null 
+             then try_cast(tot_drug_cst as float) 
+             else null 
+        end as tot_drug_cst,
+        
+        case when nullif(trim(lis_bene_cst_shr), '') is not null 
+             then try_cast(lis_bene_cst_shr as float) 
+             else null 
+        end as lis_bene_cst_shr,
+        
+        case when nullif(trim(nonlis_bene_cst_shr), '') is not null 
+             then try_cast(nonlis_bene_cst_shr as float) 
+             else null 
+        end as nonlis_bene_cst_shr,
         
         -- Senior population (65+) metrics
-        nullif(try_cast(ge65_tot_clms as integer), '') as ge65_tot_clms,
-        nullif(try_cast(ge65_tot_benes as integer), '') as ge65_tot_benes,
-        nullif(try_cast(ge65_tot_30day_fills as float), '') as ge65_tot_30day_fills,
-        nullif(try_cast(ge65_tot_drug_cst as float), '') as ge65_tot_drug_cst,
+        case when nullif(trim(ge65_tot_clms), '') is not null 
+             then try_cast(ge65_tot_clms as integer) 
+             else null 
+        end as ge65_tot_clms,
+        
+        case when nullif(trim(ge65_tot_benes), '') is not null 
+             then try_cast(ge65_tot_benes as integer) 
+             else null 
+        end as ge65_tot_benes,
+        
+        case when nullif(trim(ge65_tot_30day_fills), '') is not null 
+             then try_cast(ge65_tot_30day_fills as float) 
+             else null 
+        end as ge65_tot_30day_fills,
+        
+        case when nullif(trim(ge65_tot_drug_cst), '') is not null 
+             then try_cast(ge65_tot_drug_cst as float) 
+             else null 
+        end as ge65_tot_drug_cst,
         
         -- Drug category flags (standardized to boolean)
         case 
@@ -58,7 +99,7 @@ with raw_data as (
             else null
         end as is_antipsychotic_drug,
         
-        -- Suppression flags (standardized to boolean with null handling)
+        -- Suppression flags
         case 
             when ge65_sprsn_flag = 'Y' then true
             when ge65_sprsn_flag = 'N' then false
@@ -71,7 +112,7 @@ with raw_data as (
             else null
         end as is_ge65_beneficiary_suppressed,
         
-        -- Geographic information (hardcoded based on EDA findings)
+        -- Geographic information
         'NATIONAL' as prescriber_geo_level,
         'NATIONAL' as prescriber_geo_description
         
